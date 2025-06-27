e1562cb3a77bf6802027b6340a141fef1784eadc
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-27 09:09:28 +0200

    feat(BONUS 3): add sentence argument to display dinamically, add catch for missing sentence argument, add catch for missing secret key

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 // Endpoint URL
 const ENDPOINT = 'https://lanciweb.github.io/demo/api/pictures/';
 
+
+
+
+
 // Axios importing
 import axios from 'axios';
 
 // Chalk importing
 import chalk from 'chalk';
 
-// Simple log
-console.log(chalk.bgBlue('Hello Boolean\n'));
 
-// .env file secret key logging
-console.log(chalk.bgRed(`SECRET KEY: ${process.env.SECRET_KEY}\n`));
+
+
+
+// Get passed sentence
+const passedSentence = process.argv[2];
+
+// IF there is no passed sentence
+if (passedSentence === undefined) {
+
+    // Log error message in red
+    console.log(chalk.bgRed('\nFRASE PASSATA: nessuna.\n'));
+
+}
+// ELSE
+else {
+
+    // Log passed sentence in blue
+    console.log(chalk.bgBlue(`\nFRASE PASSATA: ${passedSentence}\n`));
+
+}
+
+
+
+
+
+// Get secret key
+const secretKey = process.env.SECRET_KEY;
+
+// IF there is no passed secret key
+if (secretKey === undefined) {
+
+    // Log error message in red
+    console.log(chalk.bgRed('CHIAVE SEGRETA: nessuna.\n'));
+
+}
+// ELSE
+else {
+
+    // Log secret key in yellow
+    console.log(chalk.bgYellow(`CHIAVE SEGRETA: ${secretKey}\n`));
+
+}
+
+
+
+
 
 // Axios fetch from endpoint
 axios.get(ENDPOINT)
@@ -22,6 +68,8 @@ axios.get(ENDPOINT)
         // Get data from response
         const data = response.data;
 
+        console.log(chalk.bgGreen('RISULTATO FETCH:\n'));
+
         // Log data
         console.log(data);
 
@@ -29,6 +77,7 @@ axios.get(ENDPOINT)
     // Catch error
     .catch(error => {
 
+        // Log error in red
         console.error(chalk.bgRed(error));
 
     });
