c2e8ec18815445ba0c69e6fc856df3357cb89caa
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-27 08:31:20 +0200

    feat(BONUS 2): add chalk and axios, log endpoint data

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-console.log('Hello Boolean');
+// Endpoint URL
+const ENDPOINT = 'https://lanciweb.github.io/demo/api/pictures/';
 
-console.log(process.env.SECRET_KEY);
+// Axios importing
+import axios from 'axios';
+
+// Chalk importing
+import chalk from 'chalk';
+
+// Simple log
+console.log(chalk.bgBlue('Hello Boolean\n'));
+
+// .env file secret key logging
+console.log(chalk.bgRed(`SECRET KEY: ${process.env.SECRET_KEY}\n`));
+
+// Axios fetch from endpoint
+axios.get(ENDPOINT)
+
+    // Process response
+    .then(response => {
+
+        // Get data from response
+        const data = response.data;
+
+        // Log data
+        console.log(data);
+
+    })
+    // Catch error
+    .catch(error => {
+
+        console.error(chalk.bgRed(error));
+
+    });
